5944db65181f62321ef1c722408ed0465b157b68
虫 <810202771@qq.com>
2018-01-11 01:26:21 +0800

    燕京中药饮片厂

--- yanjing/js/Worm.js
+++ yanjing/js/Worm.js
@@ -1,6 +1,20 @@
 /*
 * */
-
+function dataset(that) {
+    //data赋值
+    var value,text;
+    value=text=that.OriginalHTML
+    var p = eval("/\{\{(.*?)\}\}/g");
+    for(var i in that){
+        console.log(that)
+        while ((result = p.exec(text)) != null)  {
+            if(result[1].replace(/\s/g,"") == i){
+                value=value.replace(result[0],that.data[i]);
+            }
+        }
+    }
+    that.el.innerHTML = value;
+}
 //AccessNode 文本转节点
 function AccessNode(text){
     var h = text.substr(0,1);
@@ -20,37 +34,42 @@ function AccessNode(text){
 
 Worm = function (op){
     var that = this;
-    that.options = {
+    that = {
         el:"body",
         onclick:null,
         data:null,
+        OriginalHTML:null,
     }
  
-    for(var i in op)that.options[i] = op[i];
+    for(var i in op)that[i] = op[i];
 
-    that.options.el = AccessNode(that.options.el);
+    that.el = AccessNode(that.el);
+    //this.el = that.options.el
+    that.OriginalHTML = that.el.innerHTML;
 
+
+    for(var data in that.data) {
+        Object.defineProperty(this,data,{
+            set:function (val){
+                dataset(this);
+            }
+        });
+    }
 
     //执行事件
-    if(!that.options.el.length){
+    if(!that.el.length){
         //针对 ID 单个类型
-        //data赋值
-        for(var i in that.options.data){
-            var text = that.options.el.innerHTML;
-            var p = eval("/^.+?\{\{(.+?)\}\}.+?\{\{(.+?)\}\}.*$/");
-            console.log(p.exec(that.options.el.innerHTML))
-            that.options.el.innerHTML=text.replace(p, that.options.data[i]);
-            //console.log(p.exec(text),text,i,that.options.data[i])
-        }
-
+        dataset(that);
         //按下事件
-        that.options.onclick?that.options.el.onclick=function(e){that.options.onclick(e)}:null;
+        that.onclick?that.el.onclick=function(e){that.onclick(e)}:null;
     }else{
         for(var i=0;i<this.el.length;i++){
             //针对 Class or Tag 的单个类型
             //按下事件
-            that.options.onclick?that.options.el[i].onclick=function(e){that.options.onclick(e)}:null;
+            that.onclick?that.el[i].onclick=function(e){that.onclick(e)}:null;
         }
     }
+    for(var i in that)this[i] = that[i];
+
 }
 
